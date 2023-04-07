be407e0f7b31fca7d87387209654f1fb3fd89eb9
amna <amnaajward@gmail.com>
2023-04-07 21:01:47 +0530

    header UI completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,63 @@
-import "./Header.scss";
+import { useEffect, useState } from "react";
 import SearchBox from "./SearchBox";
+import "./Header.scss";
 
 function Header() {
+	const [showSearch, setShowSearch] = useState(false);
+
+	useEffect(() => {}, [showSearch]);
+
 	return (
 		<header>
-			<div className="guide">
-				<button className="btn-menu">
-					<img src="/assets/icon.svg" alt="Menu collapse" />
-				</button>
-				<a href="/">
+			<div className={`header-search ${!showSearch && "hide"}`}>
+				<div>
+					<button
+						className="menu-btn"
+						title="Back"
+						onClick={() => setShowSearch(false)}
+					>
+						<img src="assets/arrow-left.svg" alt="Menu" />
+					</button>
+				</div>
+				<SearchBox isMobile={true} />
+			</div>
+
+			<div className="header-main">
+				<div>
+					<button className="menu-btn" title="Collapse Menu">
+						<img src="assets/collapse-menu.svg" alt="Menu" />
+					</button>
 					<img
 						src="/assets/youtube-logo.png"
-						alt="YouTube logo"
 						width="110px"
+						alt="YouTube"
+						title="YouTube Home"
 					/>
-				</a>
-			</div>
-			<SearchBox />
-			<div className="nav-icons">
-				<button className="btn-create" title="create">
-					<img src="/assets/video-add.svg" alt="create video" />
-				</button>
-				<button className="btn-notifications" title="notifications">
-					<img src="/assets/notification.svg" alt="notification" />
-				</button>
-				<button className="btn-profile" title="Amna Ajward">
-					<img src="/assets/avatar.png" alt="profile" />
-				</button>
+				</div>
+
+				<div>
+					<SearchBox>
+						<button
+							title="Search"
+							className="rounded-btn show-search-btn"
+							onClick={() => setShowSearch(true)}
+						>
+							<img src="/assets/search-icon.svg" alt="show search" />
+						</button>
+					</SearchBox>
+				</div>
+
+				<div className="nav-links">
+					<button title="Create">
+						<img src="/assets/video-add.svg" alt="create" />
+					</button>
+					<button title="Notifications">
+						<img src="/assets/notification.svg" alt="notifications" />
+					</button>
+					<button className="profile-btn" id="profile">
+						<img src="/assets/avatar.png" alt="profile" />
+					</button>
+				</div>
 			</div>
 		</header>
 	);
--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,35 +1,26 @@
-function SearchBox() {
+import { ReactNode, Ref } from "react";
+import "./SearchBox.scss";
+
+interface Props {
+	children?: ReactNode;
+	isMobile?: boolean;
+}
+
+function SearchBox({ children, isMobile = false }: Props) {
 	return (
-		<div className="search-box">
+		<div className={`search-box ${isMobile && "shrink-search"}`}>
 			<div className="search-bar">
-				<img
-					src="/assets/search-icon.svg"
-					alt="Search"
-					width="20px"
-					// height="20px"
-					className="magnifier"
-				/>
-				<form action="" method="post" className="search-form">
-					<input
-						type="text"
-						name="search_query"
-						id="search-input"
-						placeholder="Search"
-					/>
-					<button id="search-button">
-						<img src="/assets/search-icon.svg" alt="Search" width="25px" />
-					</button>
+				<img src="assets/search-icon.svg" alt="" className="search-icon " />
+				<form action="">
+					<input type="text" placeholder="Search" className="search-input" />
 				</form>
 			</div>
-			<button className="search-button-narrow">
-				<img src="/assets/search-icon.svg" alt="Search" width="25px" />
+			<button className="search-btn">
+				<img src="assets/search-icon.svg" alt="" />
 			</button>
-			<button className="btn-voice-search" title="Search with your voice">
-				<img
-					src="/assets/microphone.png"
-					alt="Search with your voice"
-					width="100%"
-				/>
+			{children}
+			<button className="rounded-btn voice-search" title="Search by voice">
+				<img src="assets/microphone.png" alt="voice search" />
 			</button>
 		</div>
 	);
